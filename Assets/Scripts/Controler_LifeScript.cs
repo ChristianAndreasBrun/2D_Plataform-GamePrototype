2f67e7884b39ade902b62371961f0167de1703fd
OtherAKIRA <andreas.christian.brun@gmail.com>
2023-01-10 23:48:51 +0100

    finish

--- Assets/Scripts/Controler_LifeScript.cs
+++ Assets/Scripts/Controler_LifeScript.cs
@@ -5,43 +5,48 @@ using static Controler_LifeScript;
 
 public class Controler_LifeScript : MonoBehaviour
 {
-    public int CurrentLifes;
-    public int MaxLifes;
-    public float InvencibleTime;
-    public enum DeathMode { Teleport, ReloadScene, Destroy}
-    public DeathMode Death_Mode;
-    public Transform Respawn;
+    public int lifes_current;
+    public int lifes_max;
+    public float invencible_time;
+    public enum DeathMode { Teleport, ReloadScene, Destroy }
+    public DeathMode death_mode;
+    public Transform respawn;
+
     private Rigidbody2D Rigidbody2D;
-    private bool Invencible;
-    
+    private bool invencible;
+
+    // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
-        CurrentLifes = MaxLifes;
+        lifes_current = lifes_max;
     }
 
+
+    // Hace daño al jugador y lo pone en modo invencible
     public void Damage(int damage = 1, bool ignoreInvencible = false)
     {
-        if (!Invencible || ignoreInvencible)
+        if (!invencible || ignoreInvencible)
         {
-            CurrentLifes -= damage;
+            lifes_current -= damage;
             StartCoroutine(Invencible_Corutine());
-            if (CurrentLifes == 0)
+            if (lifes_current <= 0)
             {
                 Death();
             }
         }
+
     }
 
     public void Death()
     {
         Debug.Log("Has muerto!");
-        switch (Death_Mode)
+        switch (death_mode)
         {
             case DeathMode.Teleport:
                 Rigidbody2D.velocity = new Vector2(0, 0);
-                transform.position = Respawn.position;
-                CurrentLifes = MaxLifes;
+                transform.position = respawn.position;
+                lifes_current = lifes_max;
                 break;
             case DeathMode.ReloadScene:
                 break;
@@ -56,8 +61,8 @@ public class Controler_LifeScript : MonoBehaviour
     // Rutina para el estado de invencible
     IEnumerator Invencible_Corutine()
     {
-        Invencible = true;
-        yield return new WaitForSeconds(InvencibleTime);
-        Invencible = false;
+        invencible = true;
+        yield return new WaitForSeconds(invencible_time);
+        invencible = false;
     }
 }
